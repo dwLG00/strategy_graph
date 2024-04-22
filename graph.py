5058d49719c0d4766bcb4ef2957e0fccf335c4d3
dwLG00 <wallace.dylan00@gmail.com>
2024-04-22 00:14:27 -0500

    Updated graph dot product function

--- graph.py
+++ graph.py
@@ -5,11 +5,17 @@ def graph_normalize(graph):
     return graph * 2 - 1
 
 def graph_dot(graph):
+    # This is faster / more concise
+    transpose = np.transpose(graph)
+    return np.dot(graph, transpose)
+
+    '''
     out = np.zeros(graph.shape)
     for i in range(graph.shape[0]):
         for j in range(graph.shape[0]):
             out[i, j] = np.dot(graph[i], graph[j])
     return out
+    '''
 
 def cutoff(graph, c):
     f = lambda x: 0 if x < c else 1
