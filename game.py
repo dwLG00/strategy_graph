dcfd0ce0d12a90a1b15af9f82b8acb1ded257ea5
dwLG00 <wallace.dylan00@gmail.com>
2024-04-28 17:24:29 -0500

    pearson distance for rock paper scissors

--- game.py
+++ game.py
@@ -1,11 +1,24 @@
 import numpy as np
 import random
 
+def winrate(l):
+    return sum([1 if outcome > 0 else 0 for outcome in l]) / len(l)
+
+def bin_more_wins(l):
+    if sum([1 if outcome > 0 else 0 for outcome in l]) / 2 > len(l):
+        return True
+    return False
+
+def average_better_score(l):
+    if sum(l) > 0:
+        return True
+    return False
+
 class MatrixGame:
     def __init__(self, payout_matrix):
         self.payout_matrix = payout_matrix
 
-    def play(strategy1, strategy2, n=101, winfunc=winrate):
+    def play(self, strategy1, strategy2, n=101, winfunc=winrate):
         outcomes = []
         for _ in range(n):
             i = strategy1.play()
@@ -18,8 +31,12 @@ class MatrixGame:
         matchup_matrix = np.zeros((len(strategies), len(strategies)))
         for i, strategy1 in enumerate(strategies):
             for j, strategy2 in enumerate(strategies):
-                outcome = self.play(strategy1, strategy2, **kwargs)
-                matchup_matrix[i, j] = outcome
+                if i < j:
+                    outcome = self.play(strategy1, strategy2, **kwargs)
+                    matchup_matrix[i, j] = outcome
+                    matchup_matrix[j, i] = 1 - outcome
+                elif i == j:
+                    matchup_matrix[i, i] = 0.5
         return matchup_matrix
 
 class MonoStrategy:
@@ -42,15 +59,3 @@ class DistributionStrategy:
             rand -= p
         return len(distribution) - 1
 
-def winrate(l):
-    return sum([1 if outcome > 0 else 0]) / len(l)
-
-def bin_more_wins(l):
-    if sum([1 if outcome > 0 else 0]) / 2 > len(l):
-        return True
-    return False
-
-def average_better_score(l):
-    if sum(l) > 0:
-        return True
-    return False
--- rps.py
+++ rps.py
@@ -1,6 +1,8 @@
 import game
+import graph
 import numpy as np
 import random
+import math
 
 payoff_matrix = np.array([[0.5, 1, 0], [0, 0.5, 1], [1, 0, 0.5]])
 
@@ -8,4 +10,23 @@ def generate_distribution_strategy():
     cdf = (random.random(), random.random())
     pdf = (cdf[0], cdf[1] - cdf[0], 1 - cdf[1])
 
-    return lambda: np.random.choice([0, 1, 2], p=pdf)
+    return game.DistributionStrategy(pdf)
+
+#if __name__ == '__main__':
+N_STRATEGIES = 10
+strategies = []
+for _ in range(N_STRATEGIES):
+    strategy = generate_distribution_strategy()
+    strategies.append(strategy)
+
+mg = game.MatrixGame(payoff_matrix)
+matrix = mg.play_strategies(strategies)
+
+print(matrix)
+
+normalized = graph.graph_normalize(matrix)
+gram = graph.graph_dot(normalized)
+
+distance_matrix = np.sqrt((1 - gram / 10) / 2)
+
+print(distance_matrix)
